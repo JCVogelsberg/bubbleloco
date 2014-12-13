56124669c856ccf770c6295e272298a8f147319c
JCVogelsberg <intrepidjournalist@hotmail.com>
2014-12-13 15:08:11 -0800

    added random target color selector

--- js/script.js
+++ js/script.js
@@ -1,27 +1,53 @@
 $(document).ready(function() {
-  $('div').mouseenter(function() {
-    var wacky_size = Math.floor(Math.random()*21);
-      $(this).animate({
-        height: (wacky_size + 2) + 'em',
-        width: (wacky_size + 2) + 'em'
-      });
-    });
 
-    $('div').mouseleave(function() {
-      $(this).animate({
-        height: (wacky_size + 2) + 'em',
-        width: (wacky_size + 2) + 'em'
-      });
-    });
+$('#target_color').append(targetColor);
 
-    $('div').click(function() {
-      $(this).animate({
-        height: '0em',
-        width: '0em'
-      // });
+$('div').mouseenter(function() {
+  var wacky_size = Math.floor(Math.random()*21);
+    $(this).animate({
+      height: (wacky_size + 2) + 'em',
+      width: (wacky_size + 2) + 'em'
+    });
+  });
+
+  $('div').mouseleave(function() {
+    $(this).animate({
+      height: (wacky_size + 2) + 'em',
+      width: (wacky_size + 2) + 'em'
+    });
+  });
+
+  $('div').click(function() {
+    $(this).animate({
+      height: '0em',
+      width: '0em'
     });
   });
 });
+
+
+
+var randomColor = Math.random();
+console.log(randomColor);
+
+if (randomColor >= 0 && randomColor < 0.142) {
+    var targetColor = "RED";
+} else if (randomColor >= 0.142 && randomColor < 0.284) {
+    var targetColor = "YELLOW";
+} else if (randomColor >= 0.284 && randomColor <= 0.426) {
+    var targetColor = "BLUE";
+} else if (randomColor >= 0.426 && randomColor <= 0.568) {
+    var targetColor = "PURPLE";
+} else if (randomColor >= 0.568 && randomColor <= 0.71) {
+    var targetColor = "ORANGE";
+} else if (randomColor >= 0.71 && randomColor <= 0.852) {
+    var targetColor = "GREEN";
+} else if (randomColor >= 0.852 && randomColor <= 1) {
+    var targetColor = "BLACK";
+};
+
+
+
 
 
 
